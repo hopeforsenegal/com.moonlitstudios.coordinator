58d44c356e4176a758897fd2988fde937a304052
Kamau Vassall <kamau.vassall@gmail.com>
2024-10-14 01:32:45 -0500

    Can create and list editors

--- Editor/CoordinatorWindow.cs
+++ Editor/CoordinatorWindow.cs
@@ -21,6 +21,7 @@ public class CoordinatorWindow : EditorWindow
     public struct Events
     {
         public bool AddEditor;
+        internal bool ShowEditors;
     }
 
     public Visible visible;
@@ -30,14 +31,23 @@ public class CoordinatorWindow : EditorWindow
         var events = new Events();
         visible.HasEditors = Event.current.type == EventType.KeyDown && Event.current.keyCode == KeyCode.T ? !visible.HasEditors : visible.HasEditors;
 
-        if (visible.HasEditors) {
+        /** Render **/
+        var availableEditors = Editors.GetEditorsAvailable();
+        if (availableEditors.Length >= 2) {
             GUILayout.BeginVertical();
             {
                 GUILayout.Label("Available Editors:");
 
                 visible.Editors.ScrollPosition = EditorGUILayout.BeginScrollView(visible.Editors.ScrollPosition);
-                EditorGUILayout.EndScrollView();
 
+                foreach (var editor in availableEditors) {
+                    var editorInfo = Editors.Editor.PopulateEditorInfo(editor);
+                    GUILayout.BeginVertical();
+                    EditorGUILayout.LabelField(editorInfo.name);
+                    GUILayout.EndVertical();
+                }
+
+                EditorGUILayout.EndScrollView();
             }
             GUILayout.EndVertical();
         } else {
@@ -45,7 +55,17 @@ public class CoordinatorWindow : EditorWindow
             events.AddEditor = GUILayout.Button("Add a symlink editor");
         }
 
+        events.ShowEditors = GUILayout.Button("Show editors in Finder");
+
+        /** Events **/
         if (events.AddEditor) {
+            var path = Editors.ProjectPath;
+            var currentEditor = Editors.Editor.PopulateEditorInfo(path);
+            var newEditor = Editors.Editor.PopulateEditorInfo($"{path}Copy");
+            Editors.CreateSymlinkEditor(currentEditor, newEditor);
+        }
+        if (events.ShowEditors) {
+            System.Diagnostics.Process.Start(Editors.ProjectPath);
         }
     }
 }
---
+++ Editor/Editors.cs
@@ -0,0 +1,84 @@
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public static class Editors
+{
+    public static string ProjectPath { get; } = Application.dataPath.Replace("/Assets", "");
+    public struct Editor
+    {
+        public string name;
+        public string projectPath;
+        public string assetPath;
+        public string projectSettingsPath;
+
+        public static Editor PopulateEditorInfo(string path)
+        {
+            var pathByFolders = path.Split('/');
+            return new Editor
+            {
+                projectPath = path,
+                name = pathByFolders[pathByFolders.Length - 1],
+                assetPath = $"{path}/Assets",
+                projectSettingsPath = $"{path}/ProjectSettings"
+            };
+        }
+    }
+
+    public enum Status
+    {
+        OK
+    }
+
+    public static string[] GetEditorsAvailable()
+    {
+        var rootPath = Path.Combine(ProjectPath, "..");
+
+        return new List<string>(Directory.EnumerateDirectories(rootPath)).ToArray();
+    }
+
+    public static Status CreateSymlinkEditor(Editor original, Editor additional)
+    {
+        Directory.CreateDirectory(additional.projectPath);
+        Symlink(original.assetPath, additional.assetPath);
+        Symlink(original.projectSettingsPath, additional.projectSettingsPath);
+        return Status.OK;
+    }
+
+    private static void Symlink(string sourcePath, string destinationPath)
+    {
+        ExecuteBashCommandLine($"ln -s {sourcePath.Replace(" ", "\\ ")} {destinationPath.Replace(" ", "\\ ")}");
+    }
+
+    public static void OpenEditorPath(string editorPath)
+    {
+        UnityEngine.Debug.Assert(Directory.Exists(editorPath), "No editor at location");
+
+        Process.Start($"{EditorApplication.applicationPath}/Contents/MacOS/Unity", $"-projectPath \"{editorPath}\"");
+    }
+
+    private static void ExecuteBashCommandLine(string command)
+    {
+        using (var proc = new Process()
+        {
+            StartInfo = new ProcessStartInfo
+            {
+                FileName = "/bin/bash",
+                Arguments = $"-c \"{command.Replace("\"", "\"\"")}\"",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                CreateNoWindow = true
+            }
+        }) {
+            proc.Start();
+            proc.WaitForExit();
+
+            if (!proc.StandardError.EndOfStream) {
+                UnityEngine.Debug.LogError(proc.StandardError.ReadToEnd());
+            }
+        }
+    }
+}
