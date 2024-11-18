7cda84b94ea373314aeda40cceaaea8359b29766
Kamau Vassall <kamau.vassall@gmail.com>
2024-11-18 16:38:04 -0600

    A test project for testing out the coordinator

---
+++ Test~/Test.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    private float m_Timer;
+    protected void Start()
+    {
+#if GLOBAL_SCRIPTING_DEFINE
+        Debug.Log("Respecting the Global scripting define");
+#endif
+#if ADDITIONAL_ONLY_SCRIPTING_DEFINE
+        Debug.Log("Additional said something like intended");
+#else
+        Debug.Log("Original said something like intended");
+#endif
+        m_Timer = 20;
+    }
+
+    protected void Update()
+    {
+        if (m_Timer > 0) m_Timer -= Time.deltaTime;
+
+        if (m_Timer <= 0) {
+#if UNITY_EDITOR
+            Editors.TestComplete();
+#endif
+        }
+    }
+
+    [AfterTest]
+    public static void MethodToCallAfterPlayMode()
+    {
+        Debug.Log("This method is called after exiting Play Mode when 'TestAndPlaymode' is used");
+    }
+}
