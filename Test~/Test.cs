38f6aa3adc2beb64eb2c19e765a502b73efb6788
hopeforsenegal <kamau.vassall@gmail.com>
2024-12-10 15:10:44 -0600

    Update Test.cs

--- Test~/Test.cs
+++ Test~/Test.cs
@@ -5,13 +5,18 @@ public class Test : MonoBehaviour
     private float m_Timer;
     protected void Start()
     {
+        if (Editors.IsAdditional()) {
+            Debug.Log("Additional said something like intended");
+        }else{
+            Debug.Log("Original said something like intended");
+        }
 #if GLOBAL_SCRIPTING_DEFINE
         Debug.Log("Respecting the Global scripting define");
 #endif
 #if ADDITIONAL_ONLY_SCRIPTING_DEFINE
-        Debug.Log("Additional said something like intended");
+        Debug.Log("Additional said something else like intended");
 #else
-        Debug.Log("Original said something like intended");
+        Debug.Log("Original said something else like intended");
 #endif
         m_Timer = 20;
     }
@@ -30,6 +35,6 @@ public class Test : MonoBehaviour
     [AfterTest]
     public static void MethodToCallAfterPlayMode()
     {
-        Debug.Log("This method is called after exiting Play Mode when 'TestAndPlaymode' is used");
+        Debug.Log("This method is called after exiting Play Mode when 'Start Tests' is used");
     }
 }
